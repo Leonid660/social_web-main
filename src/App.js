0fdf0d39c0b5c58302983fca6e95f4463cbd7a57
Leonid660 <leonid_zolotovky@bk.ru>
2021-07-25 11:15:12 +0300

    add withRouter

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import {Navbar} from "./components/Navbar/Navbar";
 import {Header} from "./components/Header/Header";
 import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import {Profile} from "./components/Profile/Profile";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 
 const App = () => {
@@ -19,8 +19,8 @@ const App = () => {
                     <Route path='/dialogs'
                            render={() => <DialogsContainer
                                />}/>
-                    <Route path='/profile'
-                           render={() => <Profile/>}/>
+                    <Route path='/profile/:userId?'
+                           render={() => <ProfileContainer/>}/>
                     <Route path='/users'
                            render={() => <UsersContainer/>}/>
                 </div>
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,17 +3,16 @@ import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/preloader";
 
 export const ProfileInfo = (props) => {
-    // if(!props.profile){
-    //     return <Preloader/>
-    // }
-
+    if(!props.profile){
+        return <Preloader/>
+    }
     return <div>
         <div>
             <img
                 src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
         </div>
         <div className={s.descriptionBlock}>
-            {/*<img src={props.profile.photos.large}/>*/}
+            <img src={props.profile.photos.large}/>
             ava+descripsion
         </div>
     </div>
