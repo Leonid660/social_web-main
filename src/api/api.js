b0a7ec444307458e0b3c3d6dfe306d1a7ffcccf4
Leonid660 <leonid_zolotovky@bk.ru>
2021-07-26 20:17:15 +0300

    add thunk update Uses.jsx

--- src/api/api.js
+++ src/api/api.js
@@ -13,13 +13,20 @@ export const usersAPI = {
     getUsers(currentPage =1 ,pageSize=10){
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response=>{
-                return response.data})}
+                return response.data})},
+    follow(userId){
+       return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    },
+    unfollow(userId){
+       return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+    }
 }
-
 
 
 export const getUsers2 = (currentPage =1 ,pageSize=10) => {
     return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
-        .then(response=>{
-            return response.data})
+        .then(response => {
+            return response.data
+        })
 }
+
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import s from "./users.module.css";
 import userPhoto from "../../assets/images/172-1724489_defaultavatar-user-icon-black-png.png";
 import {NavLink} from "react-router-dom"
-import * as axios from "axios";
+
 
 let Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
@@ -33,38 +33,11 @@ let Users = (props) => {
                         {u.followed
 
                             ? <button disabled={props.followingInProgress.some(id => id === u.id)}
-                                      onClick={() => {
-                                          props.toggleFollowingProgress(true, u.id)
-                                          axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
-                                              withCredentials: true,
-                                              headers: {
-                                                  "API-KEY": "ce02fdc3-f220-462d-8381-68f1127f7d72"
-                                              }
-                                          })
-                                              .then(response => {
-
-                                                  if (response.data.resultCode == 0) {
-                                                      props.unfollow(u.id)
-                                                  }
-                                                  props.toggleFollowingProgress(false, u.id)
-                                              })
-                                      }}>Unfollow</button>
-
-                            : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                                props.toggleFollowingProgress(true, u.id)
-                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
-                                    withCredentials: true,
-                                    headers: {
-                                        "API-KEY": "ce02fdc3-f220-462d-8381-68f1127f7d72"
-                                    }
-                                })
-                                    .then(response => {
-                                        if (response.data.resultCode == 0) {
-                                            props.follow(u.id)
-                                        }
-                                        props.toggleFollowingProgress(false, u.id)
-                                    })
-                            }}>Follow</button>}
+                                      onClick={() => {props.unfollow(u.id)}}>
+                                Unfollow</button>
+                            : <button disabled={props.followingInProgress.some(id => id === u.id)}
+                                      onClick={() => {props.follow(u.id)}}>
+                                Follow</button>}
                     </div>
                 </span>
                 <span>
