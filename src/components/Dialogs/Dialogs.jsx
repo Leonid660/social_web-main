bea4e6cbc60cac28bb6fdc1c55fa046d70b9d452
Leonid660 <leonid_zolotovky@bk.ru>
2021-08-02 19:07:08 +0300

    AddMessageForm.js formsControls.js validators

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,7 @@ import s from './Dialogs.module.css'
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 import {Redirect} from "react-router-dom"
-import {Field, reduxForm} from "redux-form";
+import AddMessageForm from "./AddMessageForm";
 
 
 
@@ -12,7 +12,7 @@ export const Dialogs = (props) => {
     let state = props.dialogsPage
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messagesElements = state.messages.map(m => <Message message={m.message} id={m.id}/>)
-    let newMessagesBody = state.newMessageBody
+
 
     let addNewMessage = (values) => {
         props.sendMessage(values.newMessageBody)
@@ -27,20 +27,8 @@ export const Dialogs = (props) => {
             <div className={s.messages}>
                 <div>{messagesElements}</div>
             </div>
-           <AddMessageFormRedux onSubmit={addNewMessage}/>
+           <AddMessageForm onSubmit={addNewMessage}/>
         </div>
     )
 }
 
-const AddMessageForm = (props) => {
-    return(
-        <form onSubmit={props.handleSubmit}>
-            <div>
-               <Field component={"textarea"} name={"newMessageBody"} placeholder={"Enter your message"}/>
-            </div>
-            <div><button>Send</button></div>
-        </form>
-    )
-}
-
-const AddMessageFormRedux = reduxForm({form:"dialogAddMessageForm"})(AddMessageForm)
